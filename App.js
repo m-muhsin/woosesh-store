3d69333c2d79dc7c7bc3668bec1ecc9a963c7069
Muhammad Muhsin <muhammad.muhseen@gmail.com>
2018-10-15 10:51:56 +0530

    Loaded WooSesh Logo async

--- App.js
+++ App.js
@@ -30,16 +30,8 @@ export default class App extends React.Component {
   _loadResourcesAsync = async () => {
     return Promise.all([
       Asset.loadAsync([
-        require('./assets/images/robot-dev.png'),
-        require('./assets/images/robot-prod.png'),
+        require('./assets/images/woosesh-logo.jpg'),
       ]),
-      Font.loadAsync({
-        // This is the font that we are using for our tab bar
-        ...Icon.Ionicons.font,
-        // We include SpaceMono because we use it in HomeScreen.js. Feel free
-        // to remove this if you are not using it in your app
-        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-      }),
     ]);
   };
 
